7a55baec132b598f516c045dedc89c4fb40de84c
Mohammad Omidvar <moa39@sfu.ca>
2023-06-07 16:45:32 -0700

    Make the shell script working standalone

--- src/rs/symrustc.sh
+++ src/rs/symrustc.sh
@@ -3,9 +3,9 @@
 # SPDX-License-Identifier
 # Copyright (C) 2021-2022 Simon Fraser University (www.sfu.ca)
 
-set -euo pipefail
-
 exec $SYMRUSTC_RUSTC \
+  -Cpasses="symcc-module symcc-function" \
+  -lSymRuntime \
   -L"$SYMRUSTC_RUNTIME_DIR" \
-  -Clink-arg=-Wl,-rpath,$SYMRUSTC_RUNTIME_DIR \
+  -Clink-arg=-Wl,-rpath,"$SYMRUSTC_RUNTIME_DIR" \
   "$@"
